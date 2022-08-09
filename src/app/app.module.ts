dc266106090f9962be3312057c7ed25f62556447
Orkhan Mammadali <okus.memmedeli@gmail.com>
2022-08-09 18:45:59 +0400

    lesson 19 part 2 (toastr and filter)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,9 @@ import { TodoComponent } from './components/todo/todo.component';
 import { UserComponent } from './components/user/user.component';
 import { GetDomainNamePipe } from './pipes/get-domain-name.pipe';
 import { CommentComponent } from './components/comment/comment.component';
+import { FilterTitlePipe } from './pipes/filter-title.pipe';
 
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -23,13 +26,18 @@ import { CommentComponent } from './components/comment/comment.component';
     TodoComponent,
     UserComponent,
     GetDomainNamePipe,
-    CommentComponent
+    CommentComponent,
+    FilterTitlePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
